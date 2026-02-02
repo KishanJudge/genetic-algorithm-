82e381d9f7660e5fac12878ff94157341b62cdcb
KishanJudge <kishanjudge@hotmail.com>
2026-02-02 22:07:26 +0000

    task 8

--- base.java
+++ base.java
@@ -10,25 +10,57 @@ public class base {
         String[] population = createPopulation(bitstringSize, populationSize);
 
 
-        for (int x = 0; x < 100; x++) {
-            getScore(population[x]);
+        /*task five
+        for (int x = 0; x < populationSize; x++) {
+            score(population[x]);
             population[x] = mutation(mutationRate, bitstringSize, population[x]);
-            //System.out.println(population[x]);
         }
 
         String[] newPopulation = crossover(population, populationSize, bitstringSize);
 
-        /*for (int x = 0; x < 100; x++) {
-            System.out.println(newPopulation[x]);
-            //getScore(newPopulation[x]);
+        //task six
+        for (int x2 = 0; x2 < populationSize; x2++) {
+            if (evaluate(population, newPopulation, populationSize)) {
+                population[x2] = newPopulation[x2];
+            }
         }*/
-        
+
+
+        //task seven
+        boolean perfectCheck = isPerfect(populationSize, population, bitstringSize);
+        int generations = 0;
+
+        while (!perfectCheck) {
+
+            String[] newPopulation = crossover(population, populationSize, bitstringSize);
+
+            for (int x = 0; x < populationSize; x++) {
+                newPopulation[x] = mutation(mutationRate, bitstringSize, newPopulation[x]);
+            }
+
+            for (int x2 = 0; x2 < populationSize; x2++) {
+                if (evaluate(population[x2], newPopulation[x2], populationSize)) {
+                   population[x2] = newPopulation[x2];
+               }
+           }
+
+           generations++;
+           perfectCheck = isPerfect(populationSize, population, bitstringSize);
+        }
+
+        for (int x3 = 0; x3 < populationSize; x3++) {
+            System.out.println(population[x3]);
+        }
+
+        System.out.println("algorithm took " + generations + " generations");
+
     }
 
+    //task one
     public static String[] createPopulation(int bitstringSize, int populationSize) {
         //create a list of a hundred randomly generated bitstrings of length 𝑛 called population
         Random random = new Random();
-        int populationCheck = 0;
+        boolean perfectCheck = false;
         String[] population = new String[populationSize];
         do{
             for (int i = 0; i < population.length; i++ ) {
@@ -37,17 +69,17 @@ public class base {
                     int bit = random.nextInt(2);
                     bitstring = bitstring + String.valueOf(bit);
                 }
-                if (bitstring.equals("11111111")) {
-                    populationCheck++;
-                }
                 population[i] = bitstring;
             }
-        } while (populationCheck == 100);
+            perfectCheck = isPerfect(populationSize, population, bitstringSize);
+
+        } while (perfectCheck);
 
         return population;
     }
 
-    public static int getScore(String bitstring) {
+    //task two
+    public static int score(String bitstring) {
         int score = 0;
         for (int i = 0; i < bitstring.length(); i++) {
             char ch = bitstring.charAt(i);
@@ -58,6 +90,7 @@ public class base {
         return score;
     }
 
+    //task three
     public static String[] crossover(String[] population, int populationSize, int bitstringSize) {
         String[] newPopulation = new String[populationSize];
         for (int i = 0; i < populationSize / 2; i++) {
@@ -77,13 +110,14 @@ public class base {
         return newPopulation;
     }
 
+    //task four
     public static String mutation(int mutationRate, int bitstringSize, String bitstring) {
         Random random = new Random();
         StringBuilder newBitstring = new StringBuilder(bitstring);
 
         for (int i = 0; i < bitstringSize; i++) {
             int probability = random.nextInt(0, 100);
-            if (probability <= mutationRate) {
+            if (probability < mutationRate) {
                 if (bitstring.charAt(i) == '1') {
                     newBitstring.setCharAt(i, '0');
                 }
@@ -94,6 +128,30 @@ public class base {
         }
         String finalBitstring = newBitstring.toString();
         return finalBitstring;
+    }
+
+    //task six
+    public static boolean evaluate(String bitstring, String newBitstring, int populationSize) {
+        int parentScore = score(bitstring);
+        int offspringScore = score(newBitstring);
+        if (offspringScore > parentScore) {
+            return true;
+        }
+        return false;
+    }
+
+    //task seven
+    public static boolean isPerfect(int populationSize, String[] population, int bitstringSize) {
+        int fullPerfectCheck = 0;
+        for (int i = 0; i < populationSize; i++) {
+            if (score(population[i]) == bitstringSize) {
+                fullPerfectCheck++;
+            }
+        }
+        if (fullPerfectCheck == populationSize) {
+            return true;
+        }
+        return false;
     }
 }
 
