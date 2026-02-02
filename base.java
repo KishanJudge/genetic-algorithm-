a268c96ec1487a2a7a8421172ae1de1a256e383a
Kishan Judge <kishanjudge@hotmail.com>
2026-02-02 13:00:44 +0000

    first commit

---
+++ base.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+
+public class base {
+    
+
+    public static void createpopulation() {
+        //create a list of a hundred randomly generated bitstrings of length 𝑛 called population
+        int[] population = new int[100];
+        
+    }
+}
+    
