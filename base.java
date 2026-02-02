1599b3730cf78877298f4fa4cd6914099544d637
Kishan Judge <kishanjudge@hotmail.com>
2026-02-02 13:20:17 +0000

    made create population function

--- base.java
+++ base.java
@@ -3,11 +3,31 @@ import java.util.Random;
 
 public class base {
     
+    public static void main(String[] args) {
+        String[] population = createPopulation();
 
-    public static void createpopulation() {
-        //create a list of a hundred randomly generated bitstrings of length 𝑛 called population
-        int[] population = new int[100];
+        for (int x = 0; x < 100; x++) {
+            System.out.println(population[x]);
+        }
+
+       
         
+    }
+
+    public static String[] createPopulation() {
+        //create a list of a hundred randomly generated bitstrings of length 𝑛 called population
+        Random random = new Random();
+        int popSize = 100;
+        String[] population = new String[popSize];
+        for (int index = 0; index < population.length; index++ ) {
+            String bitstring = new String();
+            for (int x = 0; x < 8; x++) {
+                int bit = random.nextInt(2);
+                bitstring = bitstring + String.valueOf(bit);
+            }
+            population[index] = bitstring;
+        }
+        return population;
     }
 }
     
